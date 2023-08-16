b963d60d3070a04ffa3dd75920eca7f732ff8f02
codersquirrel <codersquirrelbln@gmail.com>
2023-08-16 19:03:47 +0200

    add parcel

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { Application } from "stimulus";
 import { definitionsFromContext } from "stimulus/webpack-helpers";
 
 const application = Application.start();
-const context = require.context("./controllers", true, /\.js$/);
+
+// Adjust the context path to point to the 'controllers' folder at the same level
+const context = require.context("../controllers", true, /\.js$/);
+
 application.load(definitionsFromContext(context));
